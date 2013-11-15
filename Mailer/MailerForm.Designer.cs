e7ea00d6c2c04264eb6f4134c1b1368aaeaafd56
Konstantinos Goutsos <kgoutsos@outlook.com>
2013-11-15 23:51:17 +0200

    initial commit

---
+++ Mailer/MailerForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Mailer
+{
+    partial class MailerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.send_btn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // send_btn
+            // 
+            this.send_btn.Location = new System.Drawing.Point(299, 12);
+            this.send_btn.Name = "send_btn";
+            this.send_btn.Size = new System.Drawing.Size(145, 23);
+            this.send_btn.TabIndex = 0;
+            this.send_btn.Text = "Send them all!";
+            this.send_btn.UseVisualStyleBackColor = true;
+            this.send_btn.Click += new System.EventHandler(this.send_btn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 116);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(479, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(29, 217);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(479, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Mailer.Properties.Resources.StudentGuru_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(109, -14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(335, 155);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MailerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 261);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.send_btn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "MailerForm";
+            this.Text = "Mailer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button send_btn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Mailer/MailerForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ionic.Zip;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+
+namespace Mailer
+{
+    public partial class MailerForm : Form
+    {
+        public MailerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void zip()
+        {
+            using (ZipFile zip = new ZipFile())
+            {
+                string[] filenames = System.IO.Directory.GetFileSystemEntries(Directory.GetCurrentDirectory());
+
+                progressBar1.Maximum = filenames.Length-2;
+
+                foreach (string file in filenames)
+                {
+                    
+
+                    if (file.Contains("Mailer") || (file.Contains("Ionic")))
+                        continue;
+
+                    progressBar1.Value++;
+                    progressBar1.Update();
+                    ZipEntry e = zip.AddFile(file, "");
+                    listBox1.Items.Add(file.ToString());
+                }
+
+                zip.Save("Mailer.zip");
+            }
+        }
+
+        private void send()
+        {
+            MailMessage message = new MailMessage("ece7689@upnet.gr", "stefanos990@hotmail.com");
+            Attachment data = new Attachment("Mailer.zip");
+            message.Attachments.Add(data);
+            SmtpClient client = new SmtpClient("patreas.upatras.gr");
+            client.Credentials = CredentialCache.DefaultNetworkCredentials;
+
+            client.Send(message);
+        }
+        private void send_btn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            zip();
+            //send();
+        }
+    }
+}
